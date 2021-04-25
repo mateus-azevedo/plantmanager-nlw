76d9fb103a4c4fcaf765c4751c6f69532dc2d3f3
Mateus Santos <m.santos.azeve@gmail.com>
2021-04-24 23:54:17 -0300

    componente PlantSave criado - modelo estatico

--- src/pages/PlantSelect.tsx
+++ src/pages/PlantSelect.tsx
@@ -6,6 +6,7 @@ import {
   FlatList,
   ActivityIndicator
 } from 'react-native';
+import { useNavigation } from '@react-navigation/core';
 
 import { Header } from '../components/Header';
 import { EnviromentButton } from '../components/EnviromentButton';
@@ -43,6 +44,8 @@ export function PlantSelect() {
 
   const [page, setPage] = useState(1);
   const [loadingMore, setLoadingMore] = useState(false);
+
+  const navigation = useNavigation();
 
   function handleEnviromentSelected(environment: string) {
     setEnviromentSelect(environment);
@@ -83,6 +86,10 @@ export function PlantSelect() {
     setLoadingMore(true);
     setPage(oldValue => oldValue + 1);
     fetchPlants();
+  }
+
+  function handlePlantSelect(plant: PlantProps) {
+    navigation.navigate('PlantSave');
   }
 
   useEffect(() => {
@@ -145,7 +152,10 @@ export function PlantSelect() {
           data={filteredPlants}
           keyExtractor={(item) => String(item.id)}
           renderItem={({ item }) => (
-            <PlantCardPrimary data={item} />
+            <PlantCardPrimary 
+              data={item}
+              onPress={() => handlePlantSelect(item)}
+            />
           )}
           showsVerticalScrollIndicator={false}
           numColumns={2}
