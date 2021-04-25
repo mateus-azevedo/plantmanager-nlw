bf6d3e9d1250026451514d2df2f6bd32235913de
Mateus Santos <m.santos.azeve@gmail.com>
2021-04-25 00:06:31 -0300

    TypeScript auxiliando na recuperacao de dados com useRoute

--- src/pages/PlantSave.tsx
+++ src/pages/PlantSave.tsx
@@ -11,6 +11,7 @@ import {
 } from 'react-native';
 import { SvgFromUri } from 'react-native-svg'
 import { getBottomSpace } from 'react-native-iphone-x-helper';
+import { useRoute } from '@react-navigation/core'
 
 import { Button } from '../components/Button';
 
@@ -18,22 +19,39 @@ import waterdrop from '../assets/waterdrop.png';
 import colors from '../styles/colors';
 import fonts from '../styles/fonts';
 
+interface Params {
+  plant: {
+    id: string;
+    name: string;
+    about: string;
+    water_tips: string;
+    photo: string;
+    environments: [string];
+    frequency: {
+      times: number;
+      repeat_every: string;
+    }
+  }
+}
+
 export function PlantSave() {
+  const route = useRoute();
+  const { plant } = route.params as Params;
+  
   return (
     <View style={styles.container}>
       <View style={styles.plantInfo}>
         <SvgFromUri
-          uri=""
+          uri={plant.photo}
           height={150}
           width={150}
         />
 
         <Text style={styles.plantName}>
-          Nome da Planta
+          {plant.name}
         </Text>
         <Text style={styles.plantAbout}>
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
-          Voluptate vel vero eius? Deleniti, cum perspiciatis?
+          {plant.about}
         </Text>
       </View>
 
@@ -44,7 +62,7 @@ export function PlantSave() {
             style={styles.tipImage}
           />
           <Text style={styles.tipText}>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            {plant.water_tips}
           </Text>
         </View>
 
