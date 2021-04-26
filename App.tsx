35deedf1d1071778e8df7be59ee99d42c228db2a
Mateus Santos <m.santos.azeve@gmail.com>
2021-04-25 23:41:26 -0300

    resolvendo notificacoes

--- App.tsx
+++ App.tsx
@@ -1,19 +1,40 @@
-import React from 'react';
-import { Text, View } from 'react-native';
+import React, { useEffect } from 'react';
 import AppLoading from 'expo-app-loading';
-
-import Routes from './src/routes';
+import * as Notifications from 'expo-notifications';
 import {
   useFonts,
   Jost_400Regular,
   Jost_600SemiBold
 } from '@expo-google-fonts/jost';
 
+import Routes from './src/routes';
+import { PlantProps } from './src/libs/storage';
+
 export default function App() {
   const [ fontsLoaded ] = useFonts({
     Jost_400Regular,
     Jost_600SemiBold
   });
+
+  useEffect(() => {
+    // const subscription = Notifications.addNotificationReceivedListener(
+    //   async notification => {
+    //     const data = notification.request.content.data.plant as PlantProps;
+    //     console.log(data);
+    //   });
+
+    //   return () => subscription.remove();
+
+    async function notifications() {
+      await Notifications.cancelAllScheduledNotificationsAsync();
+
+      const data = Notifications.getAllScheduledNotificationsAsync();
+      console.log("######## NOTIFICAÇÕES AGENDAS ########");
+      console.log(data);
+    }
+
+    notifications();
+  }, []);
 
   if (!fontsLoaded)
     return (
